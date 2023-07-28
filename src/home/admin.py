8841c534b2ee40ef4fff5d51173e686414280628
Zoeza <zahrahabitaa@gmail.com>
2023-07-28 21:59:33 +0100

    Update admin.py

--- src/home/admin.py
+++ src/home/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Header,SocialMedia
+from .models import Header,SocialMedia,BackgroundImg
 
 # Register your models here.
 admin.site.register(Header)
 admin.site.register(SocialMedia)
+admin.site.register(BackgroundImg)
+
 
