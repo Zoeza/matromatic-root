631cec0187bb5d9fd0e8af808ef2333370c92880
Zoeza <zahrahabitaa@gmail.com>
2023-08-01 19:01:52 +0100

    x

--- src/manager/views.py
+++ src/manager/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def dashboard(request):
+    if not request.session.get('language', None):
+        request.session['language'] = 'en-us'
+    direction = request.session.get('language')
+    url = direction + "/manager/dashboard.html"
+
+    return render(request, url, {})
+
+
+def home_manager(request, action):
+    if not request.session.get('language', None):
+        request.session['language'] = 'en-us'
+    direction = request.session.get('language')
+
+    if action == "home":
+        url = direction + "/manager/home-manager.html"
+        return render(request, url, {})
--- src/matromatic/urls.py
+++ src/matromatic/urls.py
@@ -7,5 +7,6 @@ from . import settings
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('', include('home.urls')),
+                  path('manager/', include('manager.urls')),
 
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
