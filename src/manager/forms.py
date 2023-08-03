3c21104d0c81a1b54848ad3b521ee354c01c0458
Zoeza <zahrahabitaa@gmail.com>
2023-08-03 22:03:39 +0100

    Update forms.py

--- src/manager/forms.py
+++ src/manager/forms.py
@@ -1,10 +1,10 @@
 from django.forms import ModelForm
 from django import forms
-from .models import TopPage
+from .models import HeaderPage
 
 
 class TopPageForm(ModelForm):
     class Meta:
-        model = TopPage
+        model = HeaderPage
         fields = ('language', 'head_title', 'head_text', 'company_name', 'company_slogan', 'logo_main', 'logo_head',
                   'iphone_image', 'macbook_image', 'ipad_image')
