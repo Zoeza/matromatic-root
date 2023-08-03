3385c06d6a98aa53865256244e3a1c2e6a3b96c0
Zoeza <zahrahabitaa@gmail.com>
2023-08-03 22:39:28 +0100

    Update forms.py

--- src/manager/forms.py
+++ src/manager/forms.py
@@ -6,5 +6,5 @@ from .models import TopPage
 class TopPageForm(ModelForm):
     class Meta:
         model = TopPage
-        fields = ('language', 'head_title', 'head_text', 'company_name', 'company_slogan', 'logo_main', 'logo_head',
+        fields = ('', 'head_title', 'head_text', 'company_name', 'company_slogan', 'logo_main', 'logo_head',
                   'iphone_image', 'macbook_image', 'ipad_image')
