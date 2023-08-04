20d50d1d9cf50b68623b7189e5debbd60b318038
Zoeza <zahrahabitaa@gmail.com>
2023-08-04 02:57:04 +0100

    Update views.py

--- src/manager/views.py
+++ src/manager/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from .forms import TopPageForm
-from home.models import TopPage
 
 
 def dashboard(request):
@@ -30,26 +29,7 @@ def home_manager(request, action):
     # ---------------------- top page----------------------
     if action == 'create_top_page':
         if request.method == 'POST':
-            language = request.POST.get('language', False)
-            head_title = request.POST.get(' head_title', False)
-            head_text = request.POST.get('head_text', False)
-            company_name = request.POST.get('company_name', False)
-            company_slogan = request.POST.get('company_slogan', False)
-            logo_main = request.FILES.get('logo_main')
-            logo_head = request.FILES.get('logo_head')
-            iphone_image = request.FILES.get('iphone_image')
-            macbook_image = request.FILES.get('macbook_image')
-            ipad_image = request.FILES.get('ipad_image')
-            TopPage(language=language,
-                    head_title=head_title,
-                    head_text=head_text,
-                    company_name=company_name,
-                    company_slogan=company_slogan,
-                    logo_main=logo_main,
-                    logo_head=logo_head,
-                    iphone_image=iphone_image,
-                    macbook_image=macbook_image,
-                    ipad_image=ipad_image, ).save()
-
+            top_page_form = TopPageForm(request.POST, request.FILES)
+                top_page_form.save()
         request.session['tab'] = 'top-page'
         return redirect('home-manager', 'main')
