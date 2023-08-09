980a8d2a7c8b5a7bf711f6659ffd2f01b8e66610
Zoeza <zahrahabitaa@gmail.com>
2023-08-09 23:07:47 +0100

    Update models.py

--- src/home/models.py
+++ src/home/models.py
@@ -86,7 +86,7 @@ class Service(models.Model):
         verbose_name = "service"
 
     def __str__(self):
-        return self.id
+        return self.service_title
 
 
 # ---------------------------------------------------------------------- #
