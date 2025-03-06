6c673168eb130ebfce0125bacfff6d0270a56525
Zoeza <zahrahabitaa@gmail.com>
2025-03-06 23:47:13 +0100

    lññ

--- src/matromatic/settings.py
+++ src/matromatic/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-0y(dj*jiwsr9z_$l@g6)gvx%$@e&)&3u+@(yo)h5!ry2$g&6qw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
