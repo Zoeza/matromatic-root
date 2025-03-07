af4cb7b97493e297637bd22af12386085787034d
Zoeza <zahrahabitaa@gmail.com>
2025-03-07 21:49:38 +0100

    hu

--- src/matromatic/settings.py
+++ src/matromatic/settings.py
@@ -111,10 +111,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static'
 STATIC_ROOT = '/matromatic-root/site/public/static',
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media'
 MEDIA_ROOT = '/matromatic-root/site/public/media'
 
 # Default primary key field type
