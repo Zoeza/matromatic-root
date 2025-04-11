f9ff9ef731c91a0921d852d8a935dd01528da4d5
Zoeza <zahrahabitaa@gmail.com>
2025-04-12 00:54:09 +0100

    ui

--- src/home/views.py
+++ src/home/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 import json
 import os
 
+
 def home(request):
     if not request.session.get('language', None):
         request.session['language'] = 'en-us'
@@ -18,8 +19,26 @@ def home(request):
     except json.JSONDecodeError:
         raise Http404("Erreur de lecture JSON.")
 
-    context = {
-        'data': page_data  #
-    }
+        # Initialiser les compteurs de clics pour chaque projet
+    for project_id in range(1, len(page_data['projects']['realizations']) + 1):
+        if f'clickCount_project_{project_id}' not in request.session:
+            request.session[f'clickCount_project_{project_id}'] = 0
 
-    return render(request, url, context)
+        # Gérer le clic sur un projet
+        if request.method == 'POST' and 'project_id' in request.POST:
+            project_id = request.POST['project_id']
+            if f'clickCount_project_{project_id}' in request.session:
+                request.session[f'clickCount_project_{project_id}'] += 1
+                request.session.modified = True  # Assurer que la session soit modifiée
+
+        # Passer les données des projets avec les clics dans le contexte
+        context = {
+            'data': page_data,  # Passer les données JSON
+            'projects': [
+                {'id': project['id'], 'title': project['title'],
+                 'click_count': request.session.get(f'clickCount_project_{project["id"]}', 0)}
+                for project in page_data['projects']['realizations']
+            ]
+        }
+
+        return render(request, url, context)
