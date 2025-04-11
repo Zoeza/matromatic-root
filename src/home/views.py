9e6dbfeb6577a6444e4e360c4bc223453e95aa8f
Zoeza <zahrahabitaa@gmail.com>
2025-04-11 18:26:04 +0100

    hu

--- src/home/views.py
+++ src/home/views.py
@@ -1,12 +1,19 @@
 from django.http import Http404
 from django.shortcuts import render
+import json
+import os
 
 
 def home(request):
+
     if not request.session.get('language', None):
         request.session['language'] = 'en-us'
     direction = request.session.get('language')
     url = direction + "/home/index.html"
+    json_path = os.path.join(os.path.dirname(__file__), 'data', 'page.json'
+    with open(json_path, 'r', encoding='utf-8') as file:
+        page_data = json.load(file)
+
 
     context = {
         'page': {
@@ -16,8 +23,10 @@ def home(request):
                 ("services", "Services"),
                 ("projects", "Projets"),
                 ("contact", "Contact")
-            ]
-        }
+            ],
+
+        },
+        'data' : 'page_data',
     }
 
     return render(request, url, context)
