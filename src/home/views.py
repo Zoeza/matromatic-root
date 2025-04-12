baef98eff8bee29a1f337aa80c4bef72c2d5844f
Zoeza <zahrahabitaa@gmail.com>
2025-04-12 02:36:16 +0100

    hu

--- src/home/views.py
+++ src/home/views.py
@@ -6,10 +6,10 @@ from django.views.decorators.http import require_POST
 
 
 def home(request):
-    if not request.session.get('language', None):
+    if not request.session.get('language'):
         request.session['language'] = 'en-us'
 
-    direction = request.session.get('language')
+    direction = request.session['language']
     url = direction + "/home/index.html"
     json_path = os.path.join(os.path.dirname(__file__), 'data', 'page.json')
 
@@ -21,20 +21,19 @@ def home(request):
     except json.JSONDecodeError:
         raise Http404("Erreur de lecture JSON.")
 
+    # Compteurs
     click_counts = request.session.get("click_counts", {})
     for project in page_data.get("projects", {}).get("realizations", []):
         project_id = str(project.get("id"))
         project["click_count"] = click_counts.get(project_id, 0)
 
-    # On récupère l’ID du projet cliqué depuis l’URL
-    open_modal_id = request.GET.get("project_id")
+    # Récupération du projet à ouvrir dans un modal
+    open_modal_id = request.GET.get("open_modal_id")
 
-    context = {
+    return render(request, url, {
         'data': page_data,
-        'open_modal_id': open_modal_id,
-    }
-
-    return render(request, url, context)
+        'open_modal_id': open_modal_id
+    })
 
 
 @require_POST
@@ -48,4 +47,4 @@ def increment_click(request):
     request.session["click_counts"] = click_counts
 
     # On redirige vers la page d'accueil avec l’ID du projet cliqué
-    return redirect(f"{request.path_info}?project_id={project_id}")
+    return redirect(f"/?open_modal_id={project_id}")
