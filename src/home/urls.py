ccb9a15d0d3688f50a56003dd5532f1e0e790c1e
Zoeza <zahrahabitaa@gmail.com>
2025-04-13 16:26:52 +0100

    hu

--- src/home/urls.py
+++ src/home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('increment/', views.increment_click, name='increment_click'),
     path('decrement_click/', views.decrement_click, name='decrement_click'),
     path('project_modal_content/<str:action>', views.project_modal_content, name='project_modal_content')
 
