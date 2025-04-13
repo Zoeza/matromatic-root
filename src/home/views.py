cbeba48775a8614d67760aa8ea05e287df6b4b08
Zoeza <zahrahabitaa@gmail.com>
2025-04-14 00:16:59 +0100

    hu

--- src/home/views.py
+++ src/home/views.py
@@ -77,25 +77,9 @@ def project_modal_content(request, action):
         url = direction + "/home/index.html"
 
     if action == 'add':
-        page_data = request.GET.get("page_data")
         project_id = request.GET.get("project_id")
-        if not project_id:
-            raise Http404("ID du projet manquant.")
-
-        selected_projects = request.session.get("selected_projects", [])
-
-        for p in selected_projects:
-            if str(p.get("id")) == project_id:
-                break
-        else:
-            for project in page_data.get("projects", {}).get("realizations", []):
-                if str(project.get("id")) == project_id:
-                    selected_projects.append(project)
-                    break
-
-        request.session["selected_projects"] = selected_projects
-
+        
     return render(request, url, {
-        "selected_projects": request.session.get("selected_projects", [])
+        "project_id": project_id
     })
 
