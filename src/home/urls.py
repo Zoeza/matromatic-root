db954c720d5b38ce6cc186fac634d7be1619843c
Zoeza <zahrahabitaa@gmail.com>
2025-04-14 22:27:43 +0100

    qp

--- src/home/urls.py
+++ src/home/urls.py
@@ -4,6 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('project_modal_content/<str:action>', views.project_modal_content, name='project_modal_content')
-
-
 ]
