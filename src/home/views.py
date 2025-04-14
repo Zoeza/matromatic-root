02d65e40ce7df8baadef236473cb74f55e14db19
Zoeza <zahrahabitaa@gmail.com>
2025-04-14 22:09:56 +0100

    qp

--- src/home/views.py
+++ src/home/views.py
@@ -23,10 +23,14 @@ def home(request):
         raise Http404("Erreur de lecture JSON.")
 
     if "selected_projects" not in request.session:
-        request.session["selected_projects"] = page_data.get('projects', {}).get('realizations', {})
+        request.session["selected_projects"] = {}
+
+    if "projects_list" not in request.session:
+        request.session["projects_list"] = page_data.get('projects', {}).get('realizations', {})
 
     return render(request, url, {
         'data': page_data,
+        'projects_list': request.session.get("projects_list", {})
     })
 
 
@@ -34,71 +38,32 @@ def project_modal_content(request, action):
     if not request.session.get('language'):
         request.session['language'] = 'en-us'
 
-    if "selected_projects" not in request.session:
-        request.session["selected_projects"] = []
-
     direction = request.session['language']
     url = direction + "/home/partials/content.html"
 
-    json_path = os.path.join(os.path.dirname(__file__), 'data', 'page.json')
-    try:
-        with open(json_path, 'r', encoding='utf-8') as file:
-            page_data = json.load(file)
-    except (FileNotFoundError, json.JSONDecodeError):
-        raise Http404("Erreur lors du chargement du fichier JSON.")
-
-    all_projects = page_data.get('projects', {}).get('realizations', [])
+    all_projects = request.session.get("projects_list", {})
     selected_projects = request.session["selected_projects"]
     project_id = request.GET.get("project_id", '')
 
-    if not project_id:
-        raise Http404("ID du projet manquant.")
-
     if action == 'add':
-        deja_ajoute = False
-        for project in selected_projects:
-            if str(project['id']) == project_id:
-                project['click_counts'] = project.get('click_counts', 0) + 1
-                deja_ajoute = True
-
-        if not deja_ajoute:
-            for project in all_projects:
-                if str(project['id']) == project_id:
-                    new_project = {
-                        'id': project.get('id'),
-                        'title': project.get('title'),
-                        'description': project.get('description'),
-                        'img': project.get('img'),
-                        'click_counts': 1
-                    }
-                    selected_projects.append(new_project)
-
-        # Sauvegarder les changements dans la session
-        request.session["selected_projects"] = selected_projects
-        request.session.modified = True
-
-        context = {
-            'selected_projects': selected_projects,
-        }
-        return render(request, url, context)
+        if project_id not in selected_projects:
+            selected_projects[project_id] = all_projects[project_id]
+            all_projects[project_id]['click_counts'] = 1
+        else:
+            all_projects[project_id]['click_counts'] += 1
 
     if action == 'decrement':
-        for project in selected_projects:
-            if str(project['id']) == project_id:
-                if project['click_counts'] > 1:
-                    project['click_counts'] = project.get('click_counts', 0) - 1
-                else:
-                    selected_projects.remove(project)
-        request.session["selected_projects"] = selected_projects
-        request.session.modified = True
-        return render(request, url, {"selected_projects": selected_projects})
+        if project_id in selected_projects:
+            if selected_projects[project_id].get('click_counts', 0):
+                selected_projects[project_id]['click_counts'] -= 1
+            else:
+                del selected_projects[project_id]
 
     if action == 'remove':
-        for project in selected_projects:
-            if str(project['id']) == project_id:
-                selected_projects.remove(project)
+        if project_id in selected_projects:
+            del selected_projects[project_id]
 
-        request.session["selected_projects"] = selected_projects
-        request.session.modified = True
-
-        return render(request, url, {"selected_projects": selected_projects})
+    context = {
+        'selected_projects': selected_projects,
+    }
+    return render(request, url, context)
